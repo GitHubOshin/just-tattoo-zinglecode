6d26ab1f0d7dd561de708a37d49a9c8181426951
OSHIN <izkp44@gmail.com>
2023-02-08 14:06:45 +0700

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    img: 'images/bird-tattoo.jpeg',
+    name: 'bird'
+  },
+  {
+    img: 'images/butterfly-tattoo.jpeg',
+    name: 'butterfly'
+  },
+  {
+    img: 'images/cats-tattoo.jpeg',
+    name: 'cats'
+  },
+  {
+    img: 'images/dog-tattoo.jpeg',
+    name: 'dog'
+  },
+  {
+    img: 'images/rabbit-tattoo.jpeg',
+    name: 'rabbit'
+  },
+  {
+    img: 'images/whale-tattoo.jpeg',
+    name: 'whale'
+  }
+]
+
+export default data

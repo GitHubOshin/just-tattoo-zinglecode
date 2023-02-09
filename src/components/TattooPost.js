af19501b605764a45a1053b8944c727dcd1056fd
OSHIN <izkp44@gmail.com>
2023-02-10 00:29:54 +0700

    Add prop

--- src/components/TattooPost.js
+++ src/components/TattooPost.js
@@ -1,12 +1,13 @@
 import './TattooPost.css'
 
-function TattooPost(params) {
+function TattooPost(props) {
+  const { tattoo } = props
   return (
     <div className="tattoo-post">
       <div className="tattoo-post-bg" />
       <div className="tattoo-post-content">
-        <img alt="" src="images/dog-tattoo.jpeg" />
-        <h4>Cute Dog</h4>
+        <img alt="" src={tattoo.img} />
+        <h4>{tattoo.name}</h4>
       </div>
     </div>
   )

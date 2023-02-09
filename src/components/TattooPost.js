2d81d973b68478bfeb5eb1a2f297a3afc755d226
OSHIN <izkp44@gmail.com>
2023-02-09 23:50:43 +0700

    Create TattooPost.js

---
+++ src/components/TattooPost.js
@@ -0,0 +1,15 @@
+import './TattooPost.css'
+
+function TattooPost(params) {
+  return (
+    <div className="tattoo-post">
+      <div className="tattoo-post-bg" />
+      <div className="tattoo-post-content">
+        <img alt="" src="images/dog-tattoo.jpeg" />
+        <h4>Cute Dog</h4>
+      </div>
+    </div>
+  )
+}
+
+export default TattooPost

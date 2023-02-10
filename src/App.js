58ea432aa12f7bb8606467330f0bb2f2210b6ee0
OSHIN <izkp44@gmail.com>
2023-02-10 23:58:35 +0700

    Add two events

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css'
 import data from './data'
 import Header from './components/Header'
 import TattooItem from './components/TattooItem'
@@ -6,15 +5,27 @@ import { useState } from 'react'
 import TattooPost from './components/TattooPost'
 
 function App() {
-  const [selectedTattoo, setSelectedTattoo] = useState(data[4])
+  const [selectedTattoo, setSelectedTattoo] = useState(null)
+
+  function onTattooOpen(index) {
+    setSelectedTattoo(index)
+  }
+
+  function onTattooClose(params) {
+    setSelectedTattoo(null)
+  }
 
   const tattooElements = data.map((tattoo, index) => {
-    return <TattooItem key={index} img={tattoo.img} name={tattoo.name} />
+    return (
+      <TattooItem key={index} tattoo={tattoo} onShowTattoo={onTattooOpen} />
+    )
   })
 
   let tattooPost = null
   if (!!selectedTattoo) {
-    tattooPost = <TattooPost tattoo={selectedTattoo} />
+    tattooPost = (
+      <TattooPost tattoo={selectedTattoo} onBgClick={onTattooClose} />
+    )
   }
   return (
     <div className="App">
@@ -26,3 +37,4 @@ function App() {
 }
 
 export default App
+

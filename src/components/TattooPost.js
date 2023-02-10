05a2649346ee96c25bc5c591e1c8a5a2aff6bcc9
OSHIN <izkp44@gmail.com>
2023-02-11 00:02:01 +0700

    Import CSS and get a new prop

--- src/components/TattooPost.js
+++ src/components/TattooPost.js
@@ -1,10 +1,10 @@
 import './TattooPost.css'
 
 function TattooPost(props) {
-  const { tattoo } = props
+  const { tattoo, onBgClick } = props
   return (
     <div className="tattoo-post">
-      <div className="tattoo-post-bg" />
+      <div className="tattoo-post-bg" onClick={onBgClick} />
       <div className="tattoo-post-content">
         <img alt="" src={tattoo.img} />
         <h4>{tattoo.name}</h4>
@@ -14,3 +14,4 @@ function TattooPost(props) {
 }
 
 export default TattooPost
+
